f9b8d175ae80396ac539f52b56e19e9164b2d89f
kipunaec <kipunaec@gmail.com>
2024-02-25 10:03:25 -0500

    Se agregaron nuevos archivos

--- ejResize.py
+++ ejResize.py
@@ -6,10 +6,12 @@ f, c , _ = img.shape
 porcentaje = 50
 columnas = (c*porcentaje)//100
 filas = (f*porcentaje)//100
-imgR = cv2.resize(img, (columnas, filas))
+#imgR = cv2.resize(img, (columnas, filas))
+
+imgR = cv2.resize(img,None, fx = 1.5, fy=1.5)
 
 cv2.imshow("Imagen Original", img)
 cv2.imshow("Imagen Redimensionada", imgR)
-cv2.imwrite("lechuga50.png", imgR)
+#cv2.imwrite("lechuga50.png", imgR)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
